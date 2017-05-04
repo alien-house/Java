c8ee33295be8d198d7054fa149755b5306d4e47d
alien-house <info@alien-house.com>
2017-05-04 10:55:52 -0700

    add excise

--- Loops&Arrays/src/com/company/Admission.java
+++ Loops&Arrays/src/com/company/Admission.java
@@ -7,17 +7,30 @@ import java.util.Scanner;
  */
 public class Admission {
 
-    float gradePoint = 0.0;
+    float gradePoint = 0;
     int testScore = 0;
 
     public static void main(String[] args) {
 
+        System.out.println("◆question11:Admission");
 
+        System.out.println("Please input your grade point average.");
         Scanner gpa = new Scanner(System.in);
-        gradePoint = gpa.nextFloat();
+        float gradePoint = gpa.nextFloat();
 
-        Scanner txt = new Scanner(System.in);
-        String a = txt.next();
+        System.out.println("Please input your admission test score.");
+        Scanner score = new Scanner(System.in);
+        int scorePoint = score.nextInt();
+
+        if((gradePoint >= 3.0) || (scorePoint <= 60) ){
+            System.out.println("Accept");
+
+        }else if((gradePoint <= 3.0) && (scorePoint <= 80)){
+            System.out.println("Accept");
+
+        }else{
+            System.out.println("Reject");
+        }
 
     }
 
